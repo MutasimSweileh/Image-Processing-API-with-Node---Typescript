3c003399d75cfd4ef060deb3d322d2a9e7435ce6
MutasimSweileh <mohtasmsawilh1@gmail.com>
2022-11-17 16:20:12 +0200

    Add prettier and eslint

--- README.md
+++ README.md
@@ -1,46 +1,60 @@
 ### Scripts
-- Build: ```tsc```
-- Start: ```pm run Build && node public/app.js```
-- Test: ```npm run Build && jasmine```
+
+- Build: `tsc`
+- Start: `pm run Build && node public/app.js`
+- Test: `npm run Build && jasmine`
+- Format: `prettier --write src/**/*.ts`
+- Lint: `eslint src/**/*.ts`
+- Lint:fix: `eslint --fix"`
 
 ### Usage
+
 The server will listen on port 3000 If no `PORT` variable is found in the work environment:
 
 #### Endpoint to resize images
+
 http://localhost:3000/api/images
 
 Expected query arguments are:
+
 - _filename_: the name of the image without the extension.
 - _width_: numerical pixel value > 5
 - _height_: numerical pixel value > 5
 
 #### Example 1
+
 http://localhost:3000/images
 The home page will display all uploaded images.
 
 #### Example 2
+
 http://localhost:3000/resize
 The page for uploading a new image that contains input fields to enter the width and height and upload the image.
 
 #### Example 3
+
 http://localhost:3000/resize/fjord
 Will display the original `fjord` image and input fields to enter the width and height.
 
 #### Example 4
+
 http://localhost:3000/api/images?filename=fjord&width=200&height=200
 Will scale the fjord image to 200 by 200 pixels and store the resulting image.
 On subsequent calls will serve the resized image instead of resizing the
 original again.
 
 #### Example 5
+
 http://localhost:3000/api/images?filename=fjord&width=-200&height=200
 Invalid width parameter that will be hinted to.
 
 #### Example 6
+
 http://localhost:3000/api/images?filename=fjord&width=200
 Missing height parameter that will be hinted to.
 
 ### Notes
+
 - Images are served from `assets/images`. Further images with the extension
   can be put into that directory, but the filetype is not checked.
 - Image thumbs will be stored in `assets/images/thumbnails` and can be deleted from
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,46 +1,60 @@
-import express, { Application, Request, Response } from "express";
-import Joi from "joi";
-import sharp from "sharp";
-import glob from "glob";
-import path from "path";
-import fs from "fs";
-const app = express.Router();
-const input_path = "./public/assets/images";
+import express, { Request, Response } from 'express'
+import Joi from 'joi'
+import sharp from 'sharp'
+import glob from 'glob'
+import path from 'path'
+import fs from 'fs'
+const app = express.Router()
+const input_path = './public/assets/images'
 /* The path to the output image. */
-const output_path = "./public/assets/images/thumbnails";
+const output_path = './public/assets/images/thumbnails'
 
-app.get("/api/images", (req: Request, res: Response) => {
-    const bodyOrquery = (req.query ?? req.body);
-    //console.log(postOrget);
-    const validate = Joi.object({
-        filename: Joi.string().required(),
-        width: Joi.number().min(5).required(),
-        height: Joi.number().min(5).required()
-    }).validate(bodyOrquery);
-    if (validate.error) {
-        return res.status(400).json({ success: false, message: validate.error.details[0].message });
+app.get('/api/images', (req: Request, res: Response) => {
+  const bodyOrquery = req.query ?? req.body
+  //console.log(postOrget);
+  const validate = Joi.object({
+    filename: Joi.string().required(),
+    width: Joi.number().min(5).required(),
+    height: Joi.number().min(5).required(),
+  }).validate(bodyOrquery)
+  if (validate.error) {
+    return res
+      .status(400)
+      .json({ success: false, message: validate.error.details[0].message })
+  }
+  const values = validate.value
+  const imagePath = `${input_path}/${values.filename}.*`
+  /* A function that is used to find all the files that match the pattern. */
+  glob(imagePath, (er: Error | null, files: string[]) => {
+    if (er != null) {
+      res.status(400).json({ success: false, message: er.message })
     }
-    const values = validate.value;
-    const imagePath = `${input_path}/${values.filename}.*`;
-    /* A function that is used to find all the files that match the pattern. */
-    glob(imagePath, (er: Error | null, files: string[]) => {
-        if (er != null) { res.status(400).json({ success: false, message: er.message }); }
-        if (files.length < 1) {
-            return res.status(404).json({ success: false, message: "Input image not found!" });
-        }
-        files.forEach(v => {
-            const newFile = path.parse(v);
-            const output = `${output_path}/${newFile.name}-${values.width}x${values.height + newFile.ext}`;
-            if (fs.existsSync(output)) {
-                return res.sendFile(output, { root: path.join(__dirname, "..", "..") });
-            }
-            sharp(v)
-                .resize(values.width, values.height)
-                .toFile(output)
-                .then((data) => { return res.sendFile(output, { root: path.join(__dirname, "..", "..") }) })
-                .catch((err: Error) => { return res.status(400).json({ success: false, message: err.message }); });
-        });
-    });
-});
+    if (files.length < 1) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Input image not found!' })
+    }
+    files.forEach((v) => {
+      const newFile = path.parse(v)
+      const output = `${output_path}/${newFile.name}-${values.width}x${
+        values.height + newFile.ext
+      }`
+      if (fs.existsSync(output)) {
+        return res.sendFile(output, { root: path.join(__dirname, '..', '..') })
+      }
+      sharp(v)
+        .resize(values.width, values.height)
+        .toFile(output)
+        .then(() => {
+          return res.sendFile(output, {
+            root: path.join(__dirname, '..', '..'),
+          })
+        })
+        .catch((err: Error) => {
+          return res.status(400).json({ success: false, message: err.message })
+        })
+    })
+  })
+})
 
-export default app;
+export default app
